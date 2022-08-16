d39dfc3560dc6518c2673be3fdbd52df106f12fd
wad3afdsd <109535606+wad3afdsd@users.noreply.github.com>
2022-08-17 01:40:27 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
-let stars = document.getElementById('start')
-let moon = document.getElementById('moon')
-let mountains3 = document.getElementById('mountains3')
-let mountains4 = document.getElementById('mountains4')
-let river5 = document.getElementById('river5')
-let boat6 = document.getElementById('boat6')
-window.onscroll = function(){
-    let value = scrollY;
-    stars.style.left = value + 'px';
-    moon.style.top = value + 'px';
-    mountains3.style.top = value + 'px';
+/* ======================================================================
+  Author Custom JavaScript
+====================================================================== */
+// Loop through Array of Objects
+var objPeople = [
+	{ // Object @ 0 index
+		username: "sam",
+		password: "codify"
+	},
+	{ // Object @ 1 index
+		username: "matt",
+		password: "academy"
+	},
+	{ // Object @ 2 index
+		username: "chris",
+		password: "forever"
+	}
+
+]
+
+function getInfo() {
+	var username = document.getElementById('username').value
+	var password = document.getElementById('password').value
+
+	for(var i = 0; i < objPeople.length; i++) {
+		// check is user input matches username and password of a current index of the objPeople array
+		if(username == objPeople[i].username && password == objPeople[i].password) {
+			console.log(username + " is logged in!!!")
+			// stop the function if this is found to be true
+			return
+		}
+	}
+	console.log("incorrect username or password")
 }
